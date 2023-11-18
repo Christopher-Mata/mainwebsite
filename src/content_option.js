47e79cbbe49edd887c9fe2828ae74c00ca6e3324
Christopher-Mata <christophermataaldana@gmail.com>
2023-11-17 23:12:12 -0600

    init

---
+++ src/content_option.js
@@ -0,0 +1,130 @@
+const logotext = "Christopher";
+const meta = {
+    title: "Christopher Mata", 
+    description: "I’m Christopher Mata Software Engineer _ Computer Scientist,currently working in the US and Mexico",
+};
+
+const introdata = {
+    title: "I’m Christopher Mata",
+    animated: {
+        first: "I love coding!",
+        second: "I code cool projects!",
+        third: "I develop anything and everything!",
+    },
+    description: "Hello! I am a Mexican Software Engineer, but you already knew that." + 
+    " I specialize in solving conceptual problems, quantifying them and translating them into code." + 
+    " Cool right! If you want to find out more on what I am working on, just click the portfolio button." + 
+    " I am passionate about tea, keyboards, modifying cars, and philosophy!",
+    your_img_url: "https://cdn.discordapp.com/attachments/1075239695547633786/1170791820356243486/regular_me.jpg?ex=655a53da&is=6547deda&hm=a84346851b9997d8ea3574b0a0778eacfd15afa4024bc99b347b0817ac90b7aa&",
+    // Original Image Link https://images.unsplash.com/photo-1514790193030-c89d266d5a9d
+};
+
+const dataabout = {
+    title: "Abit about my self",
+    aboutme: "I'm a passionate computer scientist and software engineer with a deep love for all things tech." +
+    " My journey in the world of technology began as a curious teenager who couldn't resist taking apart old computers to understand how they worked." + 
+    " Since then, I've embarked on an exciting and ever-evolving adventure in the realm of software development.",
+};
+const worktimeline = [{
+        jobtitle: "AI Pre-Internship",
+        where: "MKE Tech Hub Coalition",
+        date: "2020",
+    },
+    {
+        jobtitle: "Software Engineer",
+        where: "App Factory",
+        date: "2021-2022",
+    },
+    {
+        jobtitle: "Math Tutor",
+        where: "PARC",
+        date: "2021-2023",
+    },
+];
+
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "C++",
+        value: 60,
+    },
+    {
+        name: "React",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Application Development",
+        description: "Building Beautiful and Scalable Applications using the latest technologies and frameworks.",
+    },
+    {
+        title: "App Development",
+        description: "Develpment of mobile applications for Android and IOS using the latest technologies and frameworks.",
+    },
+    {
+        title: "Conceptualization and Implementation",
+        description: "bringing your ideas to life by conceptualizing and implementing them.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://cdn.discordapp.com/attachments/1075239695547633786/1170793018136207482/man-762830523.jpg?ex=655a54f7&is=6547dff7&hm=f201f7b570efd162157b51459732c72a5bab4ab6c7eaf01392560aa8602a4b5e&",
+        description: "Ambiente - A discord bot that plays 24/7 radio stations based on the user's mood.",
+        link: "https://discord.com/oauth2/authorize?client_id=1028285260951195668&permissions=8&scope=bot",
+    },
+    {
+        img: "https://repository-images.githubusercontent.com/78664391/f7e46780-6bf6-11eb-999f-8212c69d76bc",
+        description: "This React Website - A website built using React and Tailwind CSS.",
+        link: "#",
+    },
+    {
+        img: "https://asset.library.wisc.edu/1711.dl/YRUC4XB5JXC6U8F/M/h1380-4a397.jpg",
+        description: "TPA Parkside App - A mobile application to be the hub of all things Parkside.",
+        link: "https://play.google.com/store/search?q=UW%20parkside%20app&c=apps&hl=en_US&gl=US",
+    },
+    {
+        img: "https://assets-global.website-files.com/631f769e18a16a073b671850/64b59b0022ae9509d7377c54_LLM_for_life_science_2x3_grayscale.png",
+        description: "ChatBot - A LLM Chatbot that answers questions about UW Parkside.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "christophermataaldana@gmail.com",
+    YOUR_FONE: "(262)748-2034",
+    description: "Thanks for contacting me!!! I would love to converse with you and talk shop about anything and everything! Whether it be about software, cars, or job openings, I am always down to talk!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_rnq0399",
+    YOUR_TEMPLATE_ID: "template_0vja57f",
+    YOUR_USER_ID: "IlqB21_sQcpR3OCQj",
+};
+
+const socialprofils = {
+    github: "https://github.com/Christopher-Mata",
+    linkedin: "https://www.linkedin.com/in/christopher-mata1/",
+    twitter: "https://twitter.com/christ0phermata",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
